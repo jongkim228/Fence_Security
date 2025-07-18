f69cb64657a8cc789f3a6e66beb9a14d5b6e4f9e
Jonghyun Kim <146230388+jongkim228@users.noreply.github.com>
2025-07-18 13:07:34 +0000

    frame extraction script

--- scripts/extract_frames.py
+++ scripts/extract_frames.py
@@ -1,4 +1,35 @@
 import cv2
+import os
 
-def extract(video_path,output_path):
+video_path = "/workspaces/Fence_Security/videos"
+frames_dir = "/workspaces/Fence_Security/frames"
+
+def extract(video_path,frames_dir):
+    for file_name in os.listdir(video_path):
+        if file_name.endwith(".mp4"):
+            video_file = os.path.join(video_path,file_name)
+            cap = cv2.VideoCapture(video_file)
+
+            video_name = os.path.splitext(file_name)[0]
+            output_dir = os.path.join(frames_dir,video_name)
+            os.makedirs(output_dir, exist_ok=True)
+
+            count = 0
+            success = True
+            while success:
+                success, frame = cap.read()
+                frame_path = os.path.join(output_dir,f"frame{count:004d}")
+                cv2.imwrite(frame_path,frame)
+                count += 1
+
+            cap.release()
+
+extract(video_path, frames_dir)
+
+
+    
+
+
+
+
     

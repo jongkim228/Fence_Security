0c0248701a62103dfe212237efd1673585705486
Jonghyun Kim <146230388+jongkim228@users.noreply.github.com>
2025-07-23 23:19:01 +0000

    error fixed for script

--- scripts/labels_to_train.py
+++ scripts/labels_to_train.py
@@ -21,13 +21,14 @@ for sample_num in range(1,5):
             os.rename(old_path,new_path)
             i += 1
 
-sample5_list = os.listdir(SAMPLE5)
+sample5_dir = os.path.join(SAMPLE5, 'obj_train_data')
+sample5_list = os.listdir(sample5_dir)
 sample5_list.sort()
 
 i = 0
 for files in sample5_list:
     if files.endswith('.txt'):
-        old_path = os.path.join(SAMPLE5,files)
+        old_path = os.path.join(sample5_dir,files)
         new_filename = f'sample5_frame_{i:04d}.txt'
         new_path = os.path.join(VAL_OUTPUT,new_filename)
 

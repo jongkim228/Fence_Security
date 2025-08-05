e11b4bca6cfe99ebd3dd02bbd8829d2a511b200a
Jonghyun Kim <146230388+jongkim228@users.noreply.github.com>
2025-08-05 14:36:52 +0100

    fixed script and extracted frames

--- scripts/extract_frames.py
+++ scripts/extract_frames.py
@@ -1,28 +1,31 @@
 import cv2
 import os
 
-video_path = "/workspaces/Fence_Security/videos"
-frames_dir = "/workspaces/Fence_Security/frames"
+video_path = "../videos"
+frames_dir = "../frames"
 
 def extract(video_path, frames_dir):
+    sample_index = 1
     files = sorted(os.listdir(video_path))
     start_found = False
     for file_name in files:
         if not file_name.endswith(".mp4"):
             continue
-        
-        if file_name.startswith("sample6"):
-            start_found = True
-        if not start_found:
-            continue
-        
-        # 이후는 기존 코드와 동일
+
         video_file = os.path.join(video_path, file_name)
         cap = cv2.VideoCapture(video_file)
         if not cap.isOpened():
             print(f"open failed: {file_name}")
             continue
 
+
+        while True:
+            output_dir = os.path.join(frames_dir, f"sample{sample_index:01d}")
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+                break
+            else:
+                sample_index += 1
         video_name = os.path.splitext(file_name)[0]
         output_dir = os.path.join(frames_dir, video_name)
         os.makedirs(output_dir, exist_ok=True)
@@ -39,7 +42,7 @@ def extract(video_path, frames_dir):
             success, frame = cap.read()
 
         cap.release()
-
+    sample_index += 1
 extract(video_path, frames_dir)
 
 

941f83c75f13bffd78ce83c43d5251754e5e3044
Jonghyun Kim <146230388+jongkim228@users.noreply.github.com>
2025-08-13 19:41:24 +0100

    copy and paste to dataset train and val dir

---
+++ scripts/move_to_train.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+frame_dir = Path("../frames")
+label_dir = Path("../labels")
+
+output_dir_train = Path("../dataset/train")
+output_dir_val = Path("../dataset/val")
+
+for i in frame_dir.iterdir():
+    for dir in :
+    
+
